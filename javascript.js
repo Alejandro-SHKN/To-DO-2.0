e48e021da1297a962a7b673dcec22dd8ac452321
Alejandro <alejandroshkn@gmail.com>
2023-05-04 00:37:45 -0500

    Se agregaron modificaciones y correcciones al código

--- javascript.js
+++ javascript.js
@@ -38,16 +38,22 @@ let botonBorrarDiv = document.getElementById(`ID-button__borrarDiv`);
 // divPrincipal.appendChild(nuevoDiv)
 
 /*Obteniendo el valor del textarea */
-
+/*Importante recordar que value es para elementos de formulario si queremos acceder a el contenido de otros archivos usamos textcontent  */
 /* Utilizando una funcion para agregar un div hijo*/
 botonAgregarDiv.addEventListener(`click`, function () {
     let valorTextarea = document.getElementById(`ID-textarea__tarea`).value;
     if(valorTextarea == ""){
         alert(`Por favor introduce una tarea para agregar`)
     } else {
-  let nuevoDiv = document.createElement(`div`);
-  nuevoDiv.classList.add(`div__containerHijo`);
-  nuevoDiv.innerHTML = `<h2 class="h1__titulohijo 1" id="ID-h1__titulo hijo1">${valorTextarea}</h2><button class="button__borrarTarea" id="ID-button__borrarTarea">🗑️</button>`;
-  divPrincipal.appendChild(nuevoDiv);
+        let nuevoDiv = document.createElement(`div`);
+        nuevoDiv.classList.add(`div__containerHijo`);
+        nuevoDiv.innerHTML = `<h2 class="h1__titulohijo 1" id="ID-h1__titulo hijo1">${valorTextarea}</h2><button class="button__borrarTarea" id="ID-button__borrarTarea"><img class="img__basuraLogo"src="https://i.postimg.cc/MGnt1xY7/trash-bin.png" alt=""></button>`;
+        divPrincipal.appendChild(nuevoDiv);
+        
+        // Agregamos evento click al botón de borrar
+        let botonBorrar = nuevoDiv.querySelector(`#ID-button__borrarTarea`);
+        botonBorrar.addEventListener(`click`, function(){
+            this.parentElement.remove();
+        })
     }
 });
